ba33a2e9efeca35abf271c239758a1972b929fe5
AlvaroPdev <ampcastro1305@gmail.com>
2025-08-01 22:37:13 -0400

    feat: lycoris

---
+++ src/app/components/lycoris/lycoris.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lycoris',
+  imports: [],
+  templateUrl: './lycoris.component.html',
+  styleUrl: './lycoris.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LycorisComponent { }
